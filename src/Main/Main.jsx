b03eb65e20f9f6b13274584229c6c63aa22f696d
Madhin <madhinhussain@gmail.com>
2021-07-27 15:19:07 +0100

    search bar working

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,41 +1,16 @@
 import React from 'react'
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import Card from '../components/Card/Card';
+import Card from '../components/Card'
 
 
-
-const Main = () => {
-  const [beers, setBeers] = useState();
-  const [search, setSearch] = useState("");
- 
-  const apiURL = "https://api.punkapi.com/v2/beers"
-
-  const fetchData = async () => {
-    const response = await axios.get(apiURL)
-
-    setBeers(response.data)
-    console.log(beers)
-}
-
-useEffect(() => {
-fetchData()
-}, [])
-
-
-const filteredBeers = search.length === 0 ? beers : 
-    beers.filter(beer => beer.name.
-                toLowerCase().includes(search.toLowerCase()))
-
+const Main = (props) => {
+const {beers} = props;
 
   return (
     <div>
-    
       {beers &&
           beers.map((beer) => {
             return <Card beer = {beer}/>
           })}
-          
       </div>
   );
 }
