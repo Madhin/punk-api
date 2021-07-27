87665c75b33885666130e81bfedb29d9ed9cfebe
Madhin <madhinhussain@gmail.com>
2021-07-27 17:13:55 +0100

    search bar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './Main/Main';
 import NavBar from './components/Navbar';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 
 const App = () => {
+  const [beers, setBeers] = useState();
+  const [search, setSearch] = useState("");
+  
+ 
+  const apiURL = "https://api.punkapi.com/v2/beers"
+
+  const fetchData = async () => {
+    const response = await axios.get(apiURL)
+
+    setBeers(response.data)
+    console.log(beers)
+}
+
+useEffect(() => {
+fetchData()
+}, [])
+
+const filteredBeers = search.length === 0 ? beers : 
+    beers.filter(beer => beer.name.
+                toLowerCase().includes(search.toLowerCase()))
+                
+console.log(filteredBeers)
+
+
   return (
     <div>
-      <NavBar />
-      <Main />
+      <NavBar setSearch = {setSearch}/>
+      <Main beers = {filteredBeers} />
     </div>
   )
 }
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -2,12 +2,15 @@ import React, {useState} from 'react'
 import styles from './NavBar.module.scss'
 
 
-const NavBar = () => {
+
+const NavBar = (props) => {
+  const {setSearch} = props;
 
   return (
     <div className = {styles.NavBar}>
       <img src = "https://gtswiki.gt-beginners.net/decal/png/56/56/88/7062308398250885656_1.png" alt= "Logo"></img>
-      <input className = {styles.search} type="text" name="search bar" placeholder="Search beer" />
+      <input className = {styles.search} type="text" name="search bar" placeholder="Search beer" onChange = {e => setSearch(e.target.value)}/>
+     
     </div>
     
   )
