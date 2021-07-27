48f004dfc417e6e4426a9e4e1ac09ceb717ce67c
Madhin <madhinhussain@gmail.com>
2021-07-27 18:35:06 +0100

    slider bar styling

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -5,12 +5,18 @@ import styles from './NavBar.module.scss'
 
 const NavBar = (props) => {
   const {setSearch} = props;
+  const [state, setState] = useState("")
+
+  const handleChange = (e) =>{
+    setState(e.target.value)
+  }
 
   return (
     <div className = {styles.NavBar}>
       <img src = "https://gtswiki.gt-beginners.net/decal/png/56/56/88/7062308398250885656_1.png" alt= "Logo"></img>
       <input className = {styles.search} type="text" name="search bar" placeholder="Search beer" onChange = {e => setSearch(e.target.value)}/>
-     
+
+      <input id="typeinp" type="range" min="0" max="5" value={setState} onChange = {handleChange} step="1"/>
     </div>
     
   )
