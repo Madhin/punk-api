707a660910da73f565f2c12ab639c6ca59f568e3
Madhin <madhinhussain@gmail.com>
2021-07-27 19:03:43 +0100

    progress bar text

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -9,14 +9,20 @@ const NavBar = (props) => {
 
   const handleChange = (e) =>{
     setState(e.target.value)
+    console.log(state)
   }
+
+  const value = {state}
 
   return (
     <div className = {styles.NavBar}>
       <img src = "https://gtswiki.gt-beginners.net/decal/png/56/56/88/7062308398250885656_1.png" alt= "Logo"></img>
       <input className = {styles.search} type="text" name="search bar" placeholder="Search by name" onChange = {e => setSearch(e.target.value)}/>
-
-      <input id="typeinp" type="range" min="0" max="5" value={state} onChange = {handleChange} step="1"/>
+      <div className={styles.progress}>
+      <label>Filter by IBU</label>
+      <input  type="range" min="0" max="70" value={state} onChange = {handleChange} step="10"/>
+      <input className = {styles.bar} placeholder = {value.value} />
+      </div>
     </div>
     
   )
