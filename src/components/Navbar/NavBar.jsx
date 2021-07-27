2282f114c738f7799e955a6501a71b93bda1a2ef
Madhin <madhinhussain@gmail.com>
2021-07-27 18:44:25 +0100

    search bar text

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = (props) => {
   return (
     <div className = {styles.NavBar}>
       <img src = "https://gtswiki.gt-beginners.net/decal/png/56/56/88/7062308398250885656_1.png" alt= "Logo"></img>
-      <input className = {styles.search} type="text" name="search bar" placeholder="Search beer" onChange = {e => setSearch(e.target.value)}/>
+      <input className = {styles.search} type="text" name="search bar" placeholder="Search by name" onChange = {e => setSearch(e.target.value)}/>
 
       <input id="typeinp" type="range" min="0" max="5" value={state} onChange = {handleChange} step="1"/>
     </div>
