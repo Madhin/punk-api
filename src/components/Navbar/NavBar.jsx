7a12d2709fdfdd53f367f98df917291b3db8143a
Madhin <madhinhussain@gmail.com>
2021-09-03 16:18:08 +0100

    styling updated

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -18,7 +18,7 @@ const NavBar = (props) => {
 
   return (
     <div className = {styles.NavBar}>
-      <img src = "https://gtswiki.gt-beginners.net/decal/png/56/56/88/7062308398250885656_1.png" alt= "Logo"></img>
+      <img className = {styles.logo} src = "https://gtswiki.gt-beginners.net/decal/png/56/56/88/7062308398250885656_1.png" alt= "Logo"></img>
       <input className = {styles.search} type="text" name="search bar" placeholder="Search by name" 
       onChange = {e => 
       {setSearch(e.target.value);
