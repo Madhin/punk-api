65a57fe3784dc80d746ad80bd4a71618d26ac361
Madhin <madhinhussain@gmail.com>
2021-09-03 15:19:10 +0100

    sliders added

--- src/App.jsx
+++ src/App.jsx
@@ -7,36 +7,68 @@ import { useState, useEffect } from 'react';
 
 
 const App = () => {
-  const [beers, setBeers] = useState();
+  const [beers, setBeers] = useState([]);
   const [search, setSearch] = useState("");
+  const [abvVal, setAbvVal] = useState(0);
+  const [ibuVal, setIbuVal] = useState(0);
+  const [ebcVal, setEbcVal] = useState(0);
+  const [isClassic, setIsClassic] = useState(false);
   
  
-  const apiURL = "https://api.punkapi.com/v2/beers"
+//   const apiURL = "https://api.punkapi.com/v2/beers"
 
-  const fetchData = async () => {
-    const response = await axios.get(apiURL)
+//   const fetchData = async () => {
+//     const response = await axios.get(apiURL)
 
-    setBeers(response.data)
-    console.log(beers)
-}
+//     setBeers(response.data)
+//     console.log(beers)
+// }
 
-useEffect(() => {
-fetchData()
-}, [])
+// useEffect(() => {
+// fetchData()
+// }, [])
 
-const filteredBeers = search.length === 0 ? beers : 
-    beers.filter(beer => beer.name.
-                toLowerCase().includes(search.toLowerCase()))
+// const filteredBeers = search.length === 0 ? beers : 
+//     beers.filter(beer => beer.name.
+//                 toLowerCase().includes(search.toLowerCase()))
                 
-console.log(filteredBeers)
+// console.log(filteredBeers)
+
+const fetchBeers = () => {
+  const nameStr = search ? `&beer_name=${search}` : "";
+  const abvStr = `&abv_gt=${abvVal}`;
+  const ibuStr = `&ibu_gt=${ibuVal}`;
+  const ebcStr = `&ebc_gt=${ebcVal}`
+  const isClassicStr = isClassic ? `&brewed_before=01-2011` : "";
+
+  const url = `https://api.punkapi.com/v2/beers?per_page=80${nameStr}${abvStr}${ibuStr}${isClassicStr}${ebcStr}`
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => setBeers(data));
+  }
+
+  useEffect(() => {
+    fetchBeers();
+  }, []);
 
 
   return (
     <div>
-      <NavBar setSearch = {setSearch}/>
-      <Main beers = {filteredBeers} />
+      <NavBar
+      fetchBeers={fetchBeers} 
+      setSearch = {setSearch}
+      abvVal = {abvVal}
+      setAbvVal = {setAbvVal}
+      ibuVal = {ibuVal}
+      setIbuVal = {setIbuVal}
+      ebcVal = {ebcVal}
+      setEbcVal = {setEbcVal}
+      isClassic={isClassic}
+      setIsClassic={setIsClassic}/>
+      <Main beers = {beers} />
     </div>
   )
 }
+
 
 export default App
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -4,23 +4,53 @@ import styles from './NavBar.module.scss'
 
 
 const NavBar = (props) => {
-  const {setSearch} = props;
-  const [state, setState] = useState(0)
-
-  const handleChange = (e) =>{
-    setState(e.target.value)
-    console.log(state)
-  }
-
-  const value = {state}
+  const {setSearch, 
+  fetchBeers, 
+  abvVal,
+  setAbvVal,
+  ibuVal,
+  setIbuVal,
+  ebcVal,
+  setEbcVal,
+  isClassic,
+  setIsClassic} = props;
+    
 
   return (
     <div className = {styles.NavBar}>
       <img src = "https://gtswiki.gt-beginners.net/decal/png/56/56/88/7062308398250885656_1.png" alt= "Logo"></img>
-      <input className = {styles.search} type="text" name="search bar" placeholder="Search by name" onChange = {e => setSearch(e.target.value)}/>
+      <input className = {styles.search} type="text" name="search bar" placeholder="Search by name" 
+      onChange = {e => 
+      {setSearch(e.target.value);
+       fetchBeers();}
+      }/>
       <div className={styles.progress}>
       <label>Filter by IBU</label>
-      <input  type="range" min="0" max="70" value={state} onChange = {handleChange} step="10"/>
+      <label className={styles.sliderValue}>{ibuVal}</label>
+      <input  type="range" id="ibuVal" min="0" max="250" value={ibuVal} step="1" onInput={event => {
+          setIbuVal(event.target.value);
+          fetchBeers();
+          }
+        }/>
+        <label>Filter by ABV</label>
+      <label className={styles.sliderValue}>{abvVal}%</label>
+      <input  type="range" id="abvVal" min="0" max="35" value={abvVal} step="0.1" onInput={event => {
+          setAbvVal(event.target.value);
+          fetchBeers();
+          }
+        }/>
+
+      <label>Classic :</label>
+      <label>{`< 2011`}</label>
+      <input
+        type="checkbox"
+        id="isClassic"
+        onChange={() => {
+          setIsClassic(!isClassic)
+          fetchBeers();
+          }
+        }>
+      </input>
       </div>
     </div>
     
